91d16b767467616ec2a8248fd37e8165aa1de265
RobertasKirkickas <robertas3000@hotmail.com>
2024-10-03 14:45:57 +0100

    score system

--- Lab1/grading.c
+++ Lab1/grading.c
@@ -1,34 +1,38 @@
 #include <stdio.h>
 
+int getValidGrade(const char *moduleName)
+{
+    int grade;
+    do
+    {
+        printf("Enter your score for %s (0-100):\n", moduleName);
+        scanf("%d", &grade);
+        if (grade < 0 || grade > 100)
+        {
+            printf("Invalid input. Please enter a score between 0 and 100.\n");
+        }
+    } while (grade < 0 || grade > 100);
+    return grade;
+}
+
 int main()
 {
 
-    int gradeProg;
-    int gradeRDB;
-    int gradeWeb;
-    int gradeSoftw;
-    int gradeComputing;
+    int gradeProg, gradeRDB, gradeWeb, gradeSoftw, gradeComputing;
 
-    printf("Enter your score  for Advanced programming\n");
-    scanf("%d", &gradeProg);
-
-    printf("Enter your score  for Relational databases\n");
-    scanf("%d", &gradeRDB);
-
-    printf("Enter your score  for Web design\n");
-    scanf("%d", &gradeWeb);
-
-    printf("Enter your score  for Software engineering\n");
-    scanf("%d", &gradeSoftw);
-
-    printf("Enter your score  for Computing research skills\n");
-    scanf("%d", &gradeComputing);
+    // Getting valid grades from the user
+    gradeProg = getValidGrade("Advanced Programming");
+    gradeRDB = getValidGrade("Relational Databases");
+    gradeWeb = getValidGrade("Web Design");
+    gradeSoftw = getValidGrade("Software Engineering");
+    gradeComputing = getValidGrade("Computing Research Skills");
 
     printf("Advanced programming:%d\n", gradeProg);
     printf("Relational databases :%d\n", gradeRDB);
     printf("Web design :%d\n", gradeWeb);
     printf("Software engineering:%d\n", gradeSoftw);
     printf("Computing research skills:%d\n", gradeComputing);
+
     int results = (gradeProg + gradeRDB + gradeWeb + gradeSoftw + gradeComputing) / 5;
 
     if (results >= 0.0 && results <= 39.4)
@@ -52,15 +56,10 @@ int main()
         printf("First Class. Score: %d\n", results);
         printf("Awarded with prize!\n");
     }
+    else
+    {
+        printf("Something went wrong.");
+    }
 
     return 0;
 }
-
-/*
-Percentage Range Classification
-0.0% - 39.4% FAIL
-39.5% - 49.4% 3rd
-49.5% - 59.4% 2.2 (Lower Second-Class)
-59.5% - 69.4% 2.1 (Upper Second-Class)
-69.5% - 100% 1st (First-Class)
-*/
